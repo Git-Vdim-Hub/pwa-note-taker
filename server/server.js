76ec39258d39441ea39994bdef6d3c7e284ff3de
Git-Vdim-Hub <vadimrace@gmail.com>
2023-02-28 18:04:18 -0500

    dist folder generated through webpack.config.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
+//PSEUDO CODE
+/*
+1. Get app running 
+    a. add scripts, run npm install, reference mini proj
+    b. re-read chal readMe/figure out whats needed  */
+
+
+
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
